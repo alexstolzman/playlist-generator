82f96488425563e24f1a63fc04bc90a50abad00c
alexstolzman <stolzmanalex@gmail.com>
2023-03-08 16:47:49 -0600

    Added more functions, the function to create a playlist is currently not working

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,6 +5,41 @@ import axios from 'axios'
 export default function Search(){
     const [searchKey, setSearchKey]=useState("")
 
+    const createPlaylist=(artist)=>{
+         axios.post(`https://api.spotify.com//v1/users/stolzmanalex/playlists`, {
+            headers: {
+                Authorization: `BQA1-UHnS7A-Mh4WeNwT5ioEZIhhSjs5S8uA5wUT5XVJY2hvSAqMopa0BhlFdL-SmYfE7mnKiey8wfW5gxpkRiu7BNmAk9OD5NaaklWvuV0RWvDKM26U6q99CYNdAEh2yRMVIMxvPYjVX_YW0fcGjNVrIh0Y6HowkzfD-osF_vZJq3-XCq197VdfGQdEtHCtJZkZkZKifU1BJFI8F2Ylq_r8g1gDUJQ8Kr2XLzy9oZqGAGsW`,
+                ContentType: 'application/json'
+            },
+             body: JSON.stringify({
+                "name": "New Playlist",
+                "description": "New playlist description",
+                "public": true
+      }),
+        })
+    }
+
+    const getAlbumIds= (artistId)=>{
+        axios.get(`https://api.spotify.com/v1/artists/${artistId}/albums?limit=50`, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("token")}`
+            }
+        })
+        .then((response)=>{
+
+
+            response.data.items.forEach(element => {
+                ///Add tracks from every album to playlist
+            });
+
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
+    }
+
+
+
     const searchArtists = async (e) => {
     //e.preventDefault()
     axios.get("https://api.spotify.com/v1/search", {
@@ -17,11 +52,15 @@ export default function Search(){
         }
     })
     .then((response)=>{
-        console.log(response.data)
+        getAlbumIds(response.data.artists.items[0].id)
+
+        createPlaylist("test")
     })
     .catch((err)=>{
         console.log(err)
     })
+
+
 
 }
     
